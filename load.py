9b180375a8939c84539b46483de3be6d886da616
John Higgins <johnmatthiggins@gmail.com>
2024-01-02 08:07:57 -0800

    scaled image up to 10000x10000

--- load.py
+++ load.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python3
+import cv2
 import sys
 
 import numpy as np
 import plotly.express as px
 
 def main():
-    arr = np.zeros((1000, 1000), dtype=np.int32)
+    arr = np.zeros((10000, 10000), dtype=np.int32)
 
     with open(sys.argv[1], 'rb') as f:
-        for i in range(1000):
-            for j in range(1000):
+        for i in range(arr.shape[0]):
+            for j in range(arr.shape[1]):
                 byte = f.read(1)
                 arr[i, j] = int.from_bytes(byte, "big")
+    print('done reading data from file')
     
-    fig = px.imshow(arr)
-    fig.show()
+    # fig = px.imshow(arr)
+    # fig.show()
+
+    cv2.imwrite('mandelbrot.png', arr)
 
 if __name__ == '__main__':
     main()
--- mandelbrot.cc
+++ mandelbrot.cc
@@ -10,7 +10,7 @@
 
 #include "mandelbrot.h"
 
-#define LENGTH (1000)
+#define LENGTH (10000)
 
 const uint8_t PALETTE[20] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
 
